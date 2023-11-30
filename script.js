7e9ec574a417f0805d1c0b0725b8f1cb5d294336
m0h4mm3d-0 <58182483+m4hunt3r@users.noreply.github.com>
2023-11-30 14:33:45 +0100

    got it working but i cheated a lot sooo i don't know if it's count or not :(

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-const sizeOfGrid = 16;
 const container = document.querySelector('.container');
-
+const resetBtn = document.querySelector('button');
 
 const createGrid = (gridAmount) => {
+	const wrapper = document.createElement('div');
+	wrapper.classList.add('wrapper');
+
 	for (let i = 0; i < gridAmount; i++) {
 		const row = document.createElement('div');
 		row.classList.add('grid-row');
 
 		for (let j = 0; j < gridAmount; j++) {
+			//const widthAndHeight = 960 / gridAmount;
 			const gridBox = document.createElement('div');
 			gridBox.classList.add('grid-box');
+			//gridBox.style.width = `${widthAndHeight}px`;
+			//gridBox.style.height = `${widthAndHeight}px`;
 
 			gridBox.addEventListener('mouseenter', () => {
 				gridBox.style.backgroundColor = "#aaaaaa";
 			
 			})
-
 			row.appendChild(gridBox);
-
 		}
-		container.appendChild(row);
+		wrapper.appendChild(row);
 	}
-
+	container.appendChild(wrapper);
 }
 
-createGrid(sizeOfGrid);
+resetBtn.addEventListener('click', () => {
+	let userInput = Number(prompt("dim(): "));
+
+
+	while (userInput > 100) {
+		let userInput = Number(prompt("pick small dim(): "));
+	}
+	const wrapper = document.querySelector('.wrapper');
+	
+	if (!wrapper) {
+		createGrid(userInput);
+	} else {
+		wrapper.remove();
+		createGrid(userInput);
+	}
+
+})
+
+
